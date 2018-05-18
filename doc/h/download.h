b3d63a570b5b03a537189eacef1868cc008bda72
Guy Barrand <guy.barrand@gmail.com>
2018-05-18 17:35:51 +0200

    update

--- doc/h/download.h
+++ doc/h/download.h
@@ -1,33 +1,11 @@
 /**
 @page inexlib_py_download Download
 
-@section inexlib_py_download_1_0 1.0
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Darwin-x86_64-gcc_421.zip">inexlib_py-1.0-Darwin-x86_64-gcc_421.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Linux-i386-gcc_346.zip">inexlib_py-1.0-Linux-i386-gcc_346.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Linux-i386-gcc_412.zip">inexlib_py-1.0-Linux-i386-gcc_412.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Linux-i386-gcc_452.zip">inexlib_py-1.0-Linux-i386-gcc_452.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Linux-x86_64-gcc_412.zip">inexlib_py-1.0-Linux-x86_64-gcc_412.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Linux-x86_64-gcc_452.zip">inexlib_py-1.0-Linux-x86_64-gcc_452.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Linux-x86_64-icc_111.zip">inexlib_py-1.0-Linux-x86_64-icc_111.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-1.0-Windows_NT-ix86-cl_1310.zip">inexlib_py-1.0-Windows_NT-ix86-cl_1310.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py-release.apk">inexlib_py-release.apk</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py.app.zip">inexlib_py.app.zip</a>
-- <a href="../../../down_load/inexlib_py/1.0/inexlib_py.ftp">inexlib_py.ftp</a>
-
-@section inexlib_py_download_1_1 1.1
-- <a href="../../../down_load/inexlib_py/1.1/inexlib_py-mac106.app.zip">inexlib_py-mac106.app.zip</a>
-
-@section inexlib_py_download_1_2 1.2
-- <a href="../../../down_load/inexlib_py/1.2/inexlib_py-1.2-Linux-x86_64-gcc_447.zip">inexlib_py-1.2-Linux-x86_64-gcc_447.zip</a>
-- <a href="../../../down_load/inexlib_py/1.2/inexlib_py-1.2-Windows_NT-ix86-cl_1500.zip">inexlib_py-1.2-Windows_NT-ix86-cl_1500.zip</a>
-- <a href="../../../down_load/inexlib_py/1.2/inexlib_py-release.apk">inexlib_py-release.apk</a>
-- <a href="../../../down_load/inexlib_py/1.2/inexlib_py.app.zip">inexlib_py.app.zip</a>
-
-@section inexlib_py_download_data data
-- <a href="../../../down_load/inexlib_py/data/00001634_00000512_5.dst">00001634_00000512_5.dst</a>
-
-@section inexlib_py_download_videos videos
-- <a href="../../../down_load/inexlib_py/videos/inexlib_py_demo.mov">inexlib_py_demo.mov</a>
+@section inexlib_py_download_github github
+  Source and some binaries are on github :
+<p>
+  <a href="https://github.com/gbarrand/inexlib_py.git" target="_blank">https://github.com/gbarrand/inexlib_py.git</a>
+</p>
 
 @section inexlib_py_download_app_store Apple AppStore
 
@@ -35,9 +13,7 @@
 
 @section inexlib_py_download_android Android Market
 
-  inexlib_py is available on the Android Market for devices
- with at least Android-2.0. Note that the inexlib_py-release.apk
- is also available from the download area.
+  inexlib_py is available on the Android Market for devices with at least Android-2.0.
 
 @section inexlib_py_download_cocoa Cocoa (Mac desktop)
 
