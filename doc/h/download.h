01358afd0136285a9e4f499c080810144e41befe
Guy Barrand <guy.barrand@gmail.com>
2018-05-18 17:36:36 +0200

    update

--- doc/h/download.h
+++ doc/h/download.h
@@ -7,21 +7,4 @@
   <a href="https://github.com/gbarrand/inexlib_py.git" target="_blank">https://github.com/gbarrand/inexlib_py.git</a>
 </p>
 
-@section inexlib_py_download_app_store Apple AppStore
-
-  inexlib_py is available on the AppStore since January 2012.
-
-@section inexlib_py_download_android Android Market
-
-  inexlib_py is available on the Android Market for devices with at least Android-2.0.
-
-@section inexlib_py_download_cocoa Cocoa (Mac desktop)
-
-  On a Mac, download and unzip the inexlib_py.app.zip, then click
- on the inexlib_py icon (or do : open inexlib_py.app, from a terminal).
- Remember that data files are searched within the directory :
-@verbatim
-     ~/Documents/inexlib_py
-@endverbatim
-
 */
