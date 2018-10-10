b538e751d3231357f8056e22a26909533c4bd787
Guy Barrand <guy.barrand@gmail.com>
2018-10-10 12:29:47 +0200

    update

--- doc/h/download.h
+++
@@ -1,9 +0,0 @@
-/**
-@page inexlib_py_download Download
-
-  Source and some binaries are on github :
-<p>
-  <a href="https://github.com/gbarrand/inexlib_py.git" target="_blank">https://github.com/gbarrand/inexlib_py.git</a>
-</p>
-
-*/
