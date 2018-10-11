23fb3462bf12ab3c89e27a6d89baaad80fed779e
Guy Barrand <guy.barrand@gmail.com>
2018-10-11 13:46:16 +0200

    update

--- doc/h/introduction.h
+++ doc/h/introduction.h
@@ -3,7 +3,7 @@
 @page inexlib_py_introduction Introduction
 
   inexlib_py is the collection of exlib examples related to Python put in a standalone package
- and distribution.
+ and distribution. The 1.1.x contains also examples coworking with Spark.
 
   There are little applications written in C++, that activates Python through its library and
  a set of .py scripts that demonstrate how to activate inlib/exlib from a Python prompt. All
@@ -25,6 +25,22 @@
     waxml.py        # write histos at the AIDA XML file format.
     polyhedron.py   # visualise a solid/shape boolean operation.
     cfitsio_hst.py  # visualise an image in a fits file.
+@endverbatim
+
+ For Spark examples :
+@verbatim
+    spark_hello.py            # hello from the C++ inlib::spark_greet().
+    spark_h1d.py              # inlib histo.
+    spark_c3d_window.py       # do a spark action, get (x,y,z) lists,
+                              # and plot the 3D cloud with a inlib-exlib/plotter.
+    spark_c3d_gui_window.py   # the same, but by using a "gui viewer" similar
+                              # to the ioda viewer. See softinex/"Apps general behaviour"
+                              # for explanations about the button panels.
+    spark_c3d_offscreen.py    # do a spark action, get (x,y,z) lists,
+                              # and plot the 3D cloud "offscreen" by producing an out.ps
+                              # Postscript file (done with gl2ps).
+    spark_cfitsio_ntuple.py   # read a TBL in a fits file, do "ntuple projections" with Spark,
+                              # and plot with the inexlib/plotter.
 @endverbatim
 
   See the README on github for instructions on how to build, install and run.
